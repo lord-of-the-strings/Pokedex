e9e75953715bc09009e0508fa952aaac689d7a78
shiburichimasco-ship-it <shibu.richi.masco@gmail.com>
2026-03-10 14:07:38 +0530

    Implementation of Search functionality

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app=Flask(__name__)
-base_url="https://pokeapi.co/api/v2/"
+base_url="https://pokeapi.co/api/v2"
 @app.route('/')
 def home():
-    url=f"{base_url}/pokemon/pikachu"
-    response = requests.get(url)
-    if response.status_code == 200:
-        return render_template("index.html",pokemon=response.json())
+    pokemon=None
+    name = request.args.get('pokemon_name', '').lower().strip()
+    if name:
+        response = requests.get(f"{base_url}/pokemon/{name}")
+        if response.status_code == 200:
+            pokemon = response.json()
+    return render_template("index.html", pokemon=pokemon)
 if __name__ == "__main__":
     app.run(debug=True)
